c7b700ae4353a687f80e02954311bf4511fbb42c
SoladrinHub <117825210+SoladrinHub@users.noreply.github.com>
2023-07-12 01:20:08 +0100

    Add files via upload

---
+++ DASHBOARD DESIGN/js/order.js
@@ -0,0 +1,22 @@
+const Orders = [
+ {
+    productName: 'JavaScript Tutorial',
+    productNumber: '77383',
+    paymentStatus: 'Due',
+    status: 'pending'
+}, 
+{
+    productName: 'CSS full course',
+    productNumber: '78475',
+    paymentStatus: 'refunded',
+    statue: 'declined'
+
+},
+{
+    productName: 'flex-box Tutorial',
+    productNumber: '34265',
+    paymentStatus: 'paid',
+    status: 'Active'
+},
+
+]
